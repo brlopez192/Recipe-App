b22d674794bd9e276fdfd6c4b86bdcdca27eeae2
brlopez192 <brlopez192@gmail.com>
2025-04-30 04:01:55 -0400

    Exercise 2.6

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
@@ -8,11 +9,11 @@ def home(request):
     return render(request, 'recipes/recipes_home.html')
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/recipe_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/recipe_detail.html'
