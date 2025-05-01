9c5ae717add179c8e85657db9b44e1ffc3696add
brlopez192 <brlopez192@gmail.com>
2025-05-01 00:45:50 -0400

    hosts

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'aqueous-dawn-06073.herokuapp.com']
+ALLOWED_HOSTS = ['aqueous-dawn-06073.herokuapp.com']
 
 
 # Application definition
