e93f0ef49c6b4234de6fea96c74fafdd9d95f5ee
brlopez192 <brlopez192@gmail.com>
2025-05-01 01:33:00 -0400

    test

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +79,14 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback to SQLite
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
@@ -135,7 +138,7 @@ LOGIN_URL = '/login/'
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
