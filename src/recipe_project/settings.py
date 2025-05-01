a0429713bd0af38863b8091f21f0083f795e7e0c
brlopez192 <brlopez192@gmail.com>
2025-05-01 02:20:55 -0400

    test

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -128,9 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
