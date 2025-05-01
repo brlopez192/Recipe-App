7ebfe480a18f1ed8ed161e9cf541365343b749ea
brlopez192 <brlopez192@gmail.com>
2025-04-30 22:02:36 -0400

    Exercise 2.7

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import home
 from .views import RecipeListView
 from .views import RecipeDetailView
-
+from .views import recipe_search
 app_name = 'recipes'
 urlpatterns = [
     path('', home,),
+    path('search/', recipe_search, name='search'),
     path('list/', RecipeListView.as_view(), name='recipe_list'),
     path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
+   
 ]
