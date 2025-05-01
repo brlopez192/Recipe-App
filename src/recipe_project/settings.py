0b4627e650a5f41d0d748e7f82aaba12c80f84c4
brlopez192 <brlopez192@gmail.com>
2025-05-01 01:54:34 -0400

    adjustments

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -124,9 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+COLLECTSTATIC = os.environ.get('DJANGO_COLLECTSTATIC', True)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
